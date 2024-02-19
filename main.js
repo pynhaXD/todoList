f2675f6f81a5879620f002c1c65b04fa95e53d18
pynhaXD <roma151205@mail.ru>
2024-02-19 08:51:37 +0700

    fix bugs and make edit btn 2 blyat

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const addBtn = document.querySelector('.btns__add-btn')
 const inputText = document.getElementById('text')
 const inputTitle = document.querySelector('.title')
 
-
-
+const postList = document.querySelector('.list')
 
 remBtn.addEventListener('click', () => {
   inputText.value = ''
@@ -13,42 +12,88 @@ remBtn.addEventListener('click', () => {
 })
 
 
-
-const postList = document.querySelector('.list')
-
 addBtn.addEventListener('click', () => {
   if(!inputTitle.value || !inputText.value) {
     alert('Заполните все поля ввода')
     return
   }
 
-  let post = `
+  const post = `
   <div class="post-wrapper">
-    <div class="post">
-      <textarea class="post-title" cols="10" rows="1" disabled>${inputTitle.value}</textarea>
-      <textarea class="post-text" cols="5" rows="5" disabled>${inputText.value}</textarea>
+    <div class="post" data-is-open="false">
+      <textarea class="post-title text-field" cols="10" rows="1" disabled>${inputTitle.value}</textarea>
+      <textarea class="post-text text-field" cols="5" rows="5" disabled>${inputText.value}</textarea>
     </div>
     <div class="post-btns">
-      <button class="edit-btn">Редактировать</button>
-      <button class="remove-post-btn">Удалить</button>
+      <button class="edit-btn" onclick="changeEditable()">Редактировать</button>
+      <button class="remove-post-btn" onclick="cancleChanges()">Удалить</button>
     </div>
   </div>
   `
 
   postList.innerHTML += post
-
 })
 
-postList.addEventListener('click', (event) => {
-  const target = event.target;
+function removePost() {
+  const target = event.target
+  const post = target.closest('.post-wrapper')
 
-  if (target.classList.contains('remove-post-btn')) {
-    const postWrapper = target.closest('.post-wrapper');
-    postWrapper.remove();
+  post.remove();
+}
+
+function changeBtnsText(post, textEdit, textCancle) {
+  const saveBtn = post.querySelector('.edit-btn')
+  const cancelBtn = post.querySelector('.remove-post-btn')
+
+  saveBtn.textContent = textEdit
+  cancelBtn.textContent = textCancle
+}
+
+function openEditText() {
+  const target = event.target
+  const post = target.closest('.post-wrapper').children[0]
+
+  const title = post.querySelector('.post-title')
+  const text = post.querySelector('.post-text')
+
+  post.dataset.isOpen = 'true'
+
+  title.removeAttribute('disabled')
+  text.removeAttribute('disabled')
+
+  return post.nextElementSibling
+}
+
+function saveNewText() {
+  const target = event.target
+  const post = target.closest('.post-wrapper').children[0]
+
+  const title = post.querySelector('.post-title')
+  const text = post.querySelector('.post-text')
+
+
+  post.dataset.isOpen = 'false'
+
+  title.setAttribute('disabled', true)
+  text.setAttribute('disabled', true)
+
+  return post.nextElementSibling
+}
+
+function changeEditable() {
+  const target = event.target
+  const post = target.closest('.post-wrapper').children[0]
+  const status = post.dataset.isOpen
+
+  const edit = openEditText()
+  const save = saveNewText()
+
+  if(status === 'false') {
+    openEditText()
+    changeBtnsText(edit, 'Сохранить', 'Отмена')
+  } else {
+    saveNewText()
+    changeBtnsText(save, 'Редактировать', 'Удалить')
   }
 
-
-});
-
-
-
+}
